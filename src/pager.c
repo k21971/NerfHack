cac7f55a2af7ccc9b4e266e52e12afaf56d804ca
Erik Lunna <eslunna@gmail.com>
2025-03-15 22:03:57 +0100

    Fix: don't show Normally made of in obj lookup for spellbooks
    From xNetHack commit 1a9af6d64:
    
    'It was possible to discern some information about the true identities of
    spellbooks: by querying object lookup for each spellbook, the player
    could figure out which two spells would be "parchment" and "vellum"
    since object lookup would only say "Normally made of leather." for those
    spellbooks.
    
    There were already some classes defined for specifically not showing
    this because it leaks data; this just adds SPBOOK_CLASS to that
    condition. Because it uses the same code, the "Normally made of" will be
    shown for spellbook types whose appearance is already identified.'

--- src/pager.c
+++ src/pager.c
@@ -1767,8 +1767,9 @@ add_obj_info(winid datawin, struct obj *obj, short otyp, char *usr_text)
      * subject to description shuffling that includes materials. If the player
      * has already discovered this object, though, then it's fine to show the
      * material.
-     * Object classes where this may matter: rings, wands. All randomized tools
-     * share materials, and all scrolls and potions are the same material. */
+    * Object classes where this may matter: rings, wands, spellbooks (leather
+     * vs cloth vs paper...). All randomized tools share materials, and all
+     * scrolls and potions are the same material. */
 
     /* char array converting materials to strings; if this is ever needed
      * anywhere else it should be externified. Corresponds exactly to the
@@ -1789,7 +1790,7 @@ add_obj_info(winid datawin, struct obj *obj, short otyp, char *usr_text)
     /* Generally the only */
     if (!reveal_info
         && ((olet == GEM_CLASS && !is_graystone(otyp))
-            || olet == RING_CLASS || olet == WAND_CLASS))
+            || olet == RING_CLASS || olet == WAND_CLASS || olet == SPBOOK_CLASS))
         Sprintf(buf, "Material: unknown");
     else
         Sprintf(buf, "Material: %s.", mat_str);
